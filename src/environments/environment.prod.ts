6d41fd928b2b99fd488c8da6955de29fd7f584ba
swethusiast <aitc.dev@gmail.com>
2019-11-07 23:02:57 +0200

    initialization

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBn5e-NK-FHRDfxDMp8vcSD8t96Wjp0p_0',
+    authDomain: 'ecommerce-f46ac.firebaseapp.com',
+    databaseURL: 'https://ecommerce-f46ac.firebaseio.com',
+    projectId: 'ecommerce-f46ac',
+    storageBucket: 'ecommerce-f46ac.appspot.com',
+    messagingSenderId: '135101220619',
+    appId: '1:135101220619:web:2d6a786e1cbb821383440e',
+    measurementId: 'G-NBCWE0H8DW',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBn5e-NK-FHRDfxDMp8vcSD8t96Wjp0p_0',
+    authDomain: 'ecommerce-f46ac.firebaseapp.com',
+    databaseURL: 'https://ecommerce-f46ac.firebaseio.com',
+    projectId: 'ecommerce-f46ac',
+    storageBucket: 'ecommerce-f46ac.appspot.com',
+    messagingSenderId: '135101220619',
+    appId: '1:135101220619:web:2d6a786e1cbb821383440e',
+    measurementId: 'G-NBCWE0H8DW',
+  },
 };
 
 /*
